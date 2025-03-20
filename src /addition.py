b5b5f50a3d03c147026ea15412715845b2f16ee8
hyderdevops <69785941+hyderdevops@users.noreply.github.com>
2025-03-20 07:25:22 +0530

    Update addition.py

--- src /addition.py
+++ src /addition.py
@@ -1,8 +1,13 @@
 # app.py
 # This is a test commit
+
 def add(a, b):
     return a + b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+if __name__ == "__main__":
+    import pytest
+    pytest.main([__file__])
